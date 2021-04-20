799cd4a99656cbd01633d6bed229692ffde83323
mjcanan <matthew.canan@gmail.com>
2021-04-20 10:51:15 -0400

    setup for user input in order component

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -17,12 +17,30 @@ import { Product } from 'src/app/interfaces/product';
 export class OrderComponent implements OnInit {
 
   products: Product[] = [];
-  
+  // order : Order = {
+  //   orderId : 0,
+  //   phonenumber: '',
+  //   productIds : [],
+  //   dateTime : new Date(),
+  //   discount: 0,
+  //   employeeId : 0,
+  //   quantity: 0,
+  //   priceCharged: 0,
+  //   zipcode: 0,
+  // };
   //for checking if an order has been submitted
   submitted: boolean = false;
-
-  // orderDetail = [];
-  // phonenumber: string = "0";
+  order : Order = {
+    phonenumber: "888-888-8888",
+    productIds: [1,1],
+    dateTime: new Date(),
+    discount: 1.00,
+    employeeId: 1,
+    quantity: 2,
+    priceCharged: 14.50,
+    zipcode: 55501
+  }
+  
   // subtotal: number = 0;
   // tax: number = 0.00;
   // adjustment : number = 0;
@@ -30,19 +48,41 @@ export class OrderComponent implements OnInit {
   // displayedColumns = ['', 'Item', 'Description', 'Price'];
   // postOrder = {};
   // employeeid: number = 0;
-
+  
   constructor(private orderService: OrderService) { }
-
+  
   ngOnInit(): void {
     this.orderService.getProducts().subscribe((data: Product[]) => {
       this.products = data;
     });
   }
-
+  
   sendOrder() {
-    let order : Order = new Order("666-666-6666",[2], new Date(),1.35,1,2,10.50,55501); 
-    this.orderService.sendPostRequest(order).subscribe((data:any)=>{
+    //hard coded to test backend -- FIX LATER
+    //let order : Order = new Order("666-666-6666",[2], new Date(),1.35,1,2,10.50,55501); 
+    
+    // this.order.phonenumber = "888-888-8888";
+    // this.order.discount = 1.00;
+    // this.order.employeeId = 1;
+    // this.order.quantity = 2;
+    // this.order.priceCharged = 14.50;
+    // this.order.zipcode = 55501;
+
+    const orderData : Order = {
+      phonenumber: this.order.phonenumber,
+      productIds: this.order.productIds,
+      dateTime: new Date(),
+      discount: this.order.discount,
+      employeeId: this.order.employeeId,
+      quantity: this.order.quantity,
+      priceCharged: this.order.priceCharged,
+      zipcode: this.order.zipcode,
+    }
+    console.log(orderData);
+
+    this.orderService.sendPostRequest(orderData).subscribe((data:any)=>{
       console.log(data);
+      this.submitted = true;
     },
     error=> {
       console.log(error);
--- src/app/interfaces/order.ts
+++ src/app/interfaces/order.ts
@@ -1,23 +1,23 @@
-export class Order {
-    public orderId : number
-    public phonenumber : string
-    public productIds : number[]
-    public dateTime : Date
-    public discount: number
-    public employeeId : number
-    public quantity: number
-    public priceCharged: number
-    public zipcode:number
+export interface Order {
+    //orderId : number
+    phonenumber : string
+    productIds : number[]
+    dateTime : Date
+    discount: number
+    employeeId : number
+    quantity: number
+    priceCharged: number
+    zipcode:number
 
-    constructor(phonenumber: string, productIds: number[], dateTime: Date, discount: number, employeeId: number, quantity: number, priceCharged: number, zipcode: number){
+    // constructor(phonenumber: string, productIds: number[], dateTime: Date, discount: number, employeeId: number, quantity: number, priceCharged: number, zipcode: number){
 
-        this.phonenumber = phonenumber;
-        this.productIds = productIds;
-        this.dateTime = dateTime;
-        this.discount = discount;
-        this.employeeId = employeeId;
-        this.quantity = quantity;
-        this.priceCharged = priceCharged;
-        this.zipcode = zipcode;
-    }
+    //     this.phonenumber = phonenumber;
+    //     this.productIds = productIds;
+    //     this.dateTime = dateTime;
+    //     this.discount = discount;
+    //     this.employeeId = employeeId;
+    //     this.quantity = quantity;
+    //     this.priceCharged = priceCharged;
+    //     this.zipcode = zipcode;
+    // }
 }
