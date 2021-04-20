237a60e01c5688a4afec4fff31d269f743480bda
mjcanan <matthew.canan@gmail.com>
2021-04-20 09:35:17 -0400

    simple css on app component

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -6,13 +6,17 @@ import {HttpClient} from '@angular/common/http';
 })
 export class OrderService {
 
-  private API_SERVER = "";
   constructor(private httpClient: HttpClient) { }
 
   public getProducts() {
     return this.httpClient.get("http://localhost:8080/order/products");
   }
+
   public sendPostRequest(data) {
     return this.httpClient.post("http://localhost:8080/order", data);
+  }
+
+  public getOrders() {
+    return this.httpClient.get("http://localhost:8080/order/order-list");
   }
 }
