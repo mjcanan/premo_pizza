380bf0dd7dc4fe16d49b08f6ec18340bdd0f6a85
mjcanan <matthew.canan@gmail.com>
2021-04-20 08:31:28 -0400

    added menu page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderComponent } from './components/order/order.component';
+import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
+  {path: "product", component: ProductComponent},
   {path:"order", component: OrderComponent}
 ];
 
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest() {
+    return this.httpClient.get("http://localhost:8080/products");
+  }
 }
