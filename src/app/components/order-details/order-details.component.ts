9297c7f9a1c4d8e6dd04d32d33716444dd47ccb2
mjcanan <matthew.canan@gmail.com>
2021-04-21 08:38:57 -0400

    refactored order-details to use order methods

--- src/app/components/order-details/order-details.component.ts
+++ src/app/components/order-details/order-details.component.ts
@@ -12,73 +12,7 @@ import { OrderService } from 'src/app/services/order.service';
 })
 export class OrderDetailsComponent implements OnInit {
   
-  products : Product[] = [];
-  order : Order;
-  subscription;
-  subscriptionProducts;
-  updated = false;
-  
-  constructor(  private orderService: OrderService,
-    private route: ActivatedRoute,
-    private router: Router) { }
+  constructor( ) { }
     
-//COMMENTED MOST OF THIS OUT -- I DON'T THINK I NEED IT ANYMORE, BUT KEEPING IT UNTIL I INTEGRATE THE OTHER COMPONENTS
-
-    ngOnInit(): void {
-    // this.getOrderDetails(this.route.snapshot.paramMap.get('id'));
-    // //copied from order component
-    // this.subscriptionProducts = this.orderService.getProducts().subscribe((data: Product[]) => {
-    //   this.products = data;
-    //});
-  }
-/*    
-  getOrderDetails(orderId){
-    this.subscription = this.orderService.getOrderById(orderId).subscribe((data:any) => {
-      this.order = data 
-    });  
-  }
-
-  update() {
-    this.orderService.updateOrder(this.order.orderId, this.order).subscribe((data:any) => {
-      console.log(data);
-    });
-
-    this.updated = true;
-  }
-
-* *****************************************************
-*
-* COPY AND PASTED FROM ORDER COMPONENT -- BETTER SOLUTION IS TO REDIRECT TO ORDER COMPONENT, BUT WITH ORDERS PREPOPULATED
-* JUST NOT SURE HOW TO DO THAT AND NEED TO GET SOMETHING WORKING
-*
-******************************************************* *
-
-  //function to handle changes to discount 
-  subtractDiscount(){
-    let sum = 0;
-    for(let i = 0; i < this.order.productIds.length; i++){
-      sum += this.products[this.order.productIds[i]-1].price;
-    };
-    this.order.priceCharged = sum - this.order.discount;
-}
-
-  //removes items from order and adjusts priceCharged
-  removeItem(index) {
-    let id = this.order.productIds.splice(index, 1);
-    this.order.priceCharged -= this.products[id[0]-1].price;
-  }
-  
-  addToOrder(product){
-    this.order.productIds.push(product.productId);
-    this.order.quantity += 1;
-    this.order.priceCharged += product.price;
-    console.log("Product ID: " + product.productId);
-  }
-
-  ngOnDestroy() : void {
-    this.subscription.unsubscribe();
-    this.subscriptionProducts.unsubscribe();
-  }
-
-  */
+  ngOnInit(): void {}
 }
