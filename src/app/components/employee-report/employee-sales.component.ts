0d317eb129d77dd775cedc6b4cf62cd3f8eac4a1
mjcanan <matthew.canan@gmail.com>
2021-04-26 15:56:02 -0400

    resolved merge conflicts

--- src/app/components/employee-report/employee-sales.component.ts
+++ src/app/components/employee-report/employee-sales.component.ts
@@ -9,10 +9,7 @@ import { OrderService } from 'src/app/services/order.service';
   selector: 'app-employee-sales',
   templateUrl: './employee-sales.component.html',
   //styleUrls: ['./employee-sales.component.css']
-<<<<<<< HEAD
-=======
  
->>>>>>> 2ace5c505cfa2890a02ae9ef803a0b11edfad94f
 })
 export class EmployeeSalesComponent implements OnInit {
   searchTerm: number;
