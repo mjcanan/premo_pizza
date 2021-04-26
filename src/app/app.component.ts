87697ffde2912be7001d3aedb1ee93f17248aa3e
mjcanan <matthew.canan@gmail.com>
2021-04-26 12:11:53 -0400

    dropdown menu in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'premo-pizza';
+  toggleFlag = false;
+
+  public showDropdown(){
+    this.toggleFlag = !this.toggleFlag;
+  }
+
 }
